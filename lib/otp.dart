4a1a73be097d05104d1d77069abda6713076babc
Shubhanshu <75465626+shubhanshu-coder@users.noreply.github.com>
2023-04-11 00:19:08 +0530

    The main page settings

--- lib/otp.dart
+++ lib/otp.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -12,28 +14,27 @@ class MyOtp extends StatefulWidget {
 }
 
 class _MyOtpState extends State<MyOtp> {
-
   final FirebaseAuth auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     final defaultPinTheme = PinTheme(
       width: 56,
       height: 56,
-      textStyle: TextStyle(fontSize: 20, color: Color.fromRGBO(30, 60, 87, 1), fontWeight: FontWeight.w600),
+      textStyle: const TextStyle(fontSize: 20, color: Color.fromRGBO(30, 60, 87, 1), fontWeight: FontWeight.w600),
       decoration: BoxDecoration(
-        border: Border.all(color: Color.fromRGBO(234, 239, 243, 1)),
+        border: Border.all(color: const Color.fromRGBO(234, 239, 243, 1)),
         borderRadius: BorderRadius.circular(20),
       ),
     );
 
     final focusedPinTheme = defaultPinTheme.copyDecorationWith(
-      border: Border.all(color: Color.fromRGBO(114, 178, 238, 1)),
+      border: Border.all(color: const Color.fromRGBO(114, 178, 238, 1)),
       borderRadius: BorderRadius.circular(8),
     );
 
     final submittedPinTheme = defaultPinTheme.copyWith(
       decoration: defaultPinTheme.decoration?.copyWith(
-        color: Color.fromRGBO(234, 239, 243, 1),
+        color: const Color.fromRGBO(234, 239, 243, 1),
       ),
     );
 
@@ -47,11 +48,11 @@ class _MyOtpState extends State<MyOtp> {
           onPressed: (){
             Navigator.pop(context);
           },
-          icon: Icon(Icons.arrow_back_ios_rounded, color: Colors.black,),
+          icon: const Icon(Icons.arrow_back_ios_rounded, color: Colors.black,),
         ),
       ),
       body: Container(
-        margin: EdgeInsets.only(left: 25, right: 25),
+        margin: const EdgeInsets.only(left: 25, right: 25),
         alignment: Alignment.center,
         child: SingleChildScrollView(
           child: Column(
@@ -62,22 +63,25 @@ class _MyOtpState extends State<MyOtp> {
                 width: 150,
                 height: 150,
               ),
-              SizedBox(
+              const SizedBox(
                 height: 25,
               ),
 
 
-              Text('Please Enter Your mobile Number', style: TextStyle(
+              const Text('Please Enter Your mobile Number', style: TextStyle(
                   fontSize: 22, fontWeight: FontWeight.bold
               ),),
-              SizedBox(
+               Text('code send to ${MyPhone.phoneNumber}', style: const TextStyle(
+                  fontSize: 22, fontWeight: FontWeight.bold
+              ),),
+              const SizedBox(
                 height: 10,
               ),
-              Text('We need to register your phone before getting started!.', style: TextStyle(
+              const Text('We need to register your phone before getting started!.', style: TextStyle(
                 fontSize: 16 ,
               ),
                 textAlign: TextAlign.center,),
-              SizedBox(
+              const SizedBox(
                 height: 30,
               ),
           Pinput(
@@ -87,7 +91,7 @@ class _MyOtpState extends State<MyOtp> {
               code=value;
             } ,
           ),
-              SizedBox(
+              const SizedBox(
                 height: 20,
               ),
               SizedBox(
@@ -101,23 +105,48 @@ class _MyOtpState extends State<MyOtp> {
 
                       // Sign the user in (or link) with the credential
                       await auth.signInWithCredential(credential);
-                      Navigator.pushNamedAndRemoveUntil(
-                          context, "home", (route) => false);
+                      Navigator.pushNamed(
+                          context, "home");
                     }
                     catch(e){
-                      print("wrong otp");
+
                     }
                 },
-                  child: Text('Verify Phone Number'),style: ElevatedButton.styleFrom(primary: Colors.green.shade600, shape: RoundedRectangleBorder(
+                  child: const Text('Verify Phone Number'),style: ElevatedButton.styleFrom(primary: Colors.green.shade600, shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10)
                 )
                 ),),
               ),
               Row(
-                children: [TextButton(onPressed:() {
-                  Navigator.pushNamedAndRemoveUntil(context, 'phone', (route) => false);
-                }, child: Text('Edit Phone Number ?',
-                  style: TextStyle(color: Colors.black),))],
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton(onPressed:() {
+                  Navigator.pushNamed(context, 'phone');
+                }, child: const Text('Edit Phone Number ?',
+                  style: TextStyle(color: Colors.black),)),
+                  TextButton(onPressed:() async{
+                    await FirebaseAuth.instance.verifyPhoneNumber(
+                        phoneNumber: MyPhone.phoneNumber,
+                        verificationCompleted: (PhoneAuthCredential credential) {
+
+                        },
+                        verificationFailed: (FirebaseAuthException e) {
+                          // phoneNumber
+                          // country
+                        },
+                        codeSent: (String verificationId, int? resendToken) {
+                          // otp send successfully
+                          MyPhone.verify=verificationId;
+                          if(resendToken!=null)
+                          MyPhone.forceIdToken=resendToken;
+                          Navigator.pushNamed(context, "otp");
+                        },
+                        codeAutoRetrievalTimeout: (String verificationId) {
+
+                        }, forceResendingToken: MyPhone.forceIdToken);
+                  }, child: const Text('Resend code',
+                    style: TextStyle(color: Colors.black),)),
+                ],
               ),
             ],
           ),
